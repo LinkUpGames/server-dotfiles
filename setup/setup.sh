dbc64191b39c80ac2a1f17cad721380712d64d92
devy <devy@localhost.localdomain>
2025-09-05 09:05:53 -0400

    Added the setup script

--- setup/setup.sh
+++ setup/setup.sh
@@ -4,13 +4,18 @@
 echo "=== Setting up server ==="
 set -euo pipefail
 
-# Symlink the files
-ln -sf weekly-update.sh /usr/local/bin/weekly-update.sh
-ln -sf weekly-update.service /etc/systemd/system/weekly-update.service
-ln -sf weekly-update.timer /etc/systemd/system/weekly-update.timer
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Copy the files
+ln -srf weekly-update.sh /usr/local/bin/weekly-update.sh
+cp weekly-update.service /etc/systemd/system/weekly-update.service
+cp weekly-update.timer /etc/systemd/system/weekly-update.timer
 
 # Reload and set systemctl
 systemctl daemon-reload
+
+# Start the service
+systemctl start weekly-update.timer
 systemctl enable --now weekly-update.timer
 
 systemctl list-timers | grep weekly-update
