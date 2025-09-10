8728d532cd5e9692f0a41b583fa39325312807a5
mjcevallos007@gmail.com <devy@localhost.localdomain>
2025-09-10 07:52:52 -0400

    dotfiles change

--- automation/dotfiles.sh
+++ automation/dotfiles.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 
 LOGFILE="/var/log/dotfiles.log"
 
@@ -11,7 +11,7 @@ LOGFILE="/var/log/dotfiles.log"
   cd $HOME/server-dotfiles >/dev/null 2>&1
   git pull origin main >/dev/null 2>&1
 
-  source $HOME/.zprofile.zsh
+  source $HOME/profile.zsh
   source $HOME/.zshrc
 
   nvim --headless "+Lazy! sync" +qa >/dev/null 2>&1
